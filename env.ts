c2269565943f5080ff24455630792f351ac5a0fd
solstice23 <solstice233@gmail.com>
2025-01-05 01:03:14 -0500

    Update env reading

--- env.ts
+++ env.ts
@@ -7,7 +7,11 @@ try {
 	env = JSON.parse(fs.readFileSync('./env.json', 'utf8'));
 } catch (e) {
 	console.log('No env.json file found, reading from process.env');
-	env = process.env;
+	try {
+		env = JSON.parse(process.env?.CONFIG_JSON as string);
+	} catch (e) {
+		console.log('No process.env.CONFIG_JSON found! Please check your environment variables');
+	}
 }
 
 export default env;
